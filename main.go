17f64e1ac68054b2b5ca23f18f1c74e00dacc522
obadrawi <omar.elbadrawi@sap.com>
2019-09-17 12:30:06 +0200

    change main to meet standard provider main

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/hashicorp/terraform/terraform"
 	"github.com/kyma-incubator/terraform-provider-gardener/provider"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
-			return provider.Provider()
-		},
-	})
+		ProviderFunc: provider.Provider})
 }
