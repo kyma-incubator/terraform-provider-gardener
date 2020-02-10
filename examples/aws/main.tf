9154ef6bd708b698417838cce8e94a7bb484edf3
Juste Guipi <juste.guipi@sap.com>
2020-02-10 14:28:11 -0500

    update AWS example template to suit the new API version

--- examples/aws/main.tf
+++ examples/aws/main.tf
@@ -1,5 +1,5 @@
 provider "gardener" {
-  kube_file = "${file("<my-gardener-service-account-kubeconfig>")}"
+  kube_file = "${file("../kubeconfig.yaml")}"
   /*kube_file          =<<-EOT
     kind: Config
     clusters:
@@ -20,7 +20,6 @@ provider "gardener" {
           namespace: garden-<profile>
         name: garden-<profile>-robot
     current-context: garden-<profile>-robot
-
     EOT*/
 }
 
@@ -32,38 +31,34 @@ resource "gardener_shoot" "test_cluster" {
   }
 
   spec {
-    cloud {
-      profile = "aws"
-      region  = "eu-central-1"
+    cloud_profile_name  = "aws"
+    region              = "eu-central-1"
+    secret_binding_name = "<secret_binding>"
 
-      secret_binding_ref {
-        name = "<secret_binding>"
+    networking {
+      type     = "calico"
+      nodes    = "10.250.0.0/19"
+      pods     = "100.96.0.0/11"
+      services = "100.64.0.0/13"
+    }
+
+    provider {
+      type = "aws"
+      worker {
+        name            = "cpu-worker"
+        max_surge       = 1
+        max_unavailable = 0
+        maximum         = 2
+        minimum         = 2
+        volume {
+          size = "50Gi"
+          type = "Standard_LRS"
+        }
+        machine {
+          type    = "m5.large"
+        }
       }
 
-      aws {
-        networks {
-          vpc {
-            cidr = "10.250.0.0/16"
-          }
-
-          internal = ["10.250.112.0/22"]
-          public   = ["10.250.96.0/22"]
-          workers  = ["10.250.0.0/19"]
-        }
-
-        worker {
-          name            = "cpu-worker"
-          machine_type    = "m5.large"
-          auto_scaler_min = 3
-          auto_scaler_max = 4
-          max_surge       = 1
-          max_unavailable = 0
-          volume_type     = "gp2"
-          volume_size     = "50Gi"
-        }
-
-        zones = ["eu-central-1a"]
-      }
     }
 
     kubernetes {
@@ -71,3 +66,4 @@ resource "gardener_shoot" "test_cluster" {
     }
   }
 }
+
